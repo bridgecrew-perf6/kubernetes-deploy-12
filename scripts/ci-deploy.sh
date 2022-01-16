4de076da1fa41046020842263e9a063d3ee9b5a5
eyadev <46867140+eyadev@users.noreply.github.com>
2022-01-16 16:58:33 +0100

    Update ci-deploy.sh

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -13,7 +13,7 @@ export COMMIT_SHA1=$COMMIT_SHA1
 envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
 mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 
-(echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode) > cert.crt
+echo -n "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
   --kubeconfig=/dev/null \
